9022bb5effc0bc248a1f5be3e2b86f4811cc6dcd
Peter <pcandell23@gmail.com>
2020-06-23 18:26:33 -0700

    Starting fresh

---
+++ SendAR/SendAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendAR
+//
+//  Created by Peter Candell on 6/23/20.
+//  Copyright © 2020 SendAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
