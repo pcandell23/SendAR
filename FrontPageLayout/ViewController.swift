a30d10d746171ac48c4379875b8ee6d85dbb3f76
Bennett Baker <bennettjbaker@gmail.com>
2020-06-28 13:56:56 -0700

    adding in all of the front page files

---
+++ FrontPageLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrontPageLayout
+//
+//  Created by Bennett Baker on 6/27/20.
+//  Copyright © 2020 Bennett Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
