c500d1aabbfbb32191d40b8316cf194a5c62aa04
Bennett Baker <bennettjbaker@gmail.com>
2020-07-02 16:38:00 -0700

    added nearby areas tab to map page

---
+++ SendAR/ViewControllers/NearbyAreas.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyAreas.swift
+//  SendAR
+//
+//  Created by Bennett Baker on 7/2/20.
+//  Copyright © 2020 Bennett Baker. All rights reserved.
+//
+
+import UIKit
+
+class NearbyAreasViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
