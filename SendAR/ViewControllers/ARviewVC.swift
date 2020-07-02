b3782c72539abea3da8a8efa1f1c4c9169a3b2c6
Bennett Baker <bennettjbaker@gmail.com>
2020-07-01 18:28:46 -0700

    location services checked and enabled for map and ar view, map centers on user, refresh location button works, location service access denied alerts added with option to open settings

--- SendAR/ViewControllers/ARviewVC.swift
+++ SendAR/ViewControllers/ARviewVC.swift
@@ -7,11 +7,114 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ARViewController: UIViewController {
     
+    let locationManager = CLLocationManager()
+    let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+        
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                print("Settings opened: \(success)") // prints true
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationServices()
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        print("Location manager set up")
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            // sets up the location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+            print("Checking location")
+        } else {
+            // show alert letting user know they have to turn this on
+            locationServiceDeniedAlert()
+            print("Locations disabled")
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+            print("Case: authorized when in use")
+            break
+            
+        case .denied:
+            // show alert instructing them how to turn on permissions
+            locationServiceDeniedAlert()
+            print("Case: location service denied")
+            break
+            
+        case .notDetermined:
+            // request permission
+            locationManager.requestWhenInUseAuthorization()
+            print("Case: requesting location authorization")
+            break
+            
+        case .restricted:
+            // show alert that user can't change this setting (parental controls)
+            locationServiceRestrictedAlert()
+            print("Case: location restricted")
+            break
+            
+        case .authorizedAlways:
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+            print("Case: authorized always")
+            break
+            
+        @unknown default:
+            //covers future cases with break to avoid crash
+            break
+            
+        }
+    }
+    
+    func locationServiceDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Denied", message: "Please enable location services for SendAR in your device settings", preferredStyle: .alert)
+        // settings button
+        alert.addAction(settingsAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    func locationServiceRestrictedAlert() {
+        let alert = UIAlertController(title: "Location Services Restricted", message: "Location services are restricted on this device", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
+    
+}
+
+extension ARViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let userLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let userAltitude = location.altitude
+        // probably want heading in here too. Note: it is part of CLLocationManager not CLLocation
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    checkLocationAuthorization()
     }
     
 }
