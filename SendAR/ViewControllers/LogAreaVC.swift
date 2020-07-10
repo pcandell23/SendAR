b3aa84c2c55c0f8711644d7497b6e5ba42b0cf1e
Bennett Baker <bennettjbaker@gmail.com>
2020-07-10 15:28:47 -0700

    log new area should work

--- SendAR/ViewControllers/LogAreaVC.swift
+++ SendAR/ViewControllers/LogAreaVC.swift
@@ -8,25 +8,83 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class LogAreaVC: UIViewController, MKMapViewDelegate {
 
+    let delegate = AppDelegate.shared()
+    
+    var defaultFenceRadius = 100
+    
+    var locationCheck: LocationChecker
+    let regionInMeters: Double = 500
+    
+    var newAreaName: String? = nil
+    var newAreaCenter: CLLocation
+    
+    var latitude: Double
+    var longitude: Double
+    
+    var latitudeString: String? = nil
+    var longitudeString: String? = nil
+    
     @IBAction func cancelButton(_ sender: Any) {
         //dismiss page
     }
     
     @IBOutlet weak var areaName: UITextField!
-    
     @IBOutlet weak var areaMap: MKMapView!
     
     @IBAction func confirmNewArea(_ sender: Any) {
         //save area region and dismiss
+        newAreaName = areaName.text
+        //IDK how to pass this info out but this is the final info needed
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        centerViewOnUserLocation()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    init(locationCheck: LocationChecker, newAreaCenter: CLLocation, latitude: Double, longitude: Double) {
+        self.locationCheck = locationCheck
+        self.newAreaCenter = newAreaCenter
+        self.latitude = latitude
+        self.longitude = longitude
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.locationCheck = LocationChecker()
+        self.newAreaCenter = CLLocation()
+        self.latitude = 0.0
+        self.longitude = 0.0
+        super.init(coder: aDecoder)
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationCheck.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            areaMap.setRegion(region, animated: true)
+        }
+    }
+    
+    func getCenterLocation(for areaMap: MKMapView) {
+        latitude = areaMap.centerCoordinate.latitude
+        longitude = areaMap.centerCoordinate.longitude
+        
+        latitudeString = latitude.description
+        longitudeString = longitude.description
+    }
+    
+    func storeNewAreaInfo(area: Area?){
+        let newArea = NSEntityDescription.insertNewObject(forEntityName: "Area", into: delegate.dataController!.persistentContainer.viewContext) as! Area
+        
+        newArea.setInitialValues(name: newAreaName, fenceLatitude: latitudeString, fenceLongitude: longitudeString, fenceRadius: Int64(defaultFenceRadius), subAreas: nil, superArea: nil)
+        
+        delegate.dataController!.saveContext()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
