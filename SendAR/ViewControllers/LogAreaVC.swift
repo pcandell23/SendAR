a0b475b15efe26460afbc31842060f1969336a18
Bennett Baker <bennettjbaker@gmail.com>
2020-07-10 11:47:44 -0700

    added LogNewArea

---
+++ SendAR/ViewControllers/LogAreaVC.swift
@@ -0,0 +1,43 @@
+//
+//  LogAreaVC.swift
+//  SendAR
+//
+//  Created by Bennett Baker on 7/10/20.
+//  Copyright © 2020 Bennett Baker. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LogAreaVC: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var areaName: UITextField!
+    
+    @IBOutlet weak var areaMap: MKMapView!
+    
+    @IBAction func confirmNewArea(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        areaName.resignFirstResponder()
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
