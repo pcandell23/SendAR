cae877aa641af666dc186c3376ba290687f6be96
Peter Candell <pcandell23@gmail.com>
2020-07-16 13:21:30 -0700

    Added ARCL library

--- SendAR/ViewControllers/ARviewVC.swift
+++ SendAR/ViewControllers/ARviewVC.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+import CoreLocation
+import MapKit
 
 class ARViewController: UIViewController {
     
     var locationCheck: LocationChecker
+    var sceneLocationView = SceneLocationView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationCheck.checkLocationServices()
+        
+        sceneLocationView.run()
+        view.addSubview(sceneLocationView)
+        
+        createLocNode()
+    }
+    
+    override func viewDidLayoutSubviews() {
+      super.viewDidLayoutSubviews()
+
+      sceneLocationView.frame = view.bounds
     }
     
     init(locationCheck: LocationChecker) {
@@ -25,5 +41,17 @@ class ARViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         self.locationCheck = LocationChecker()
         super.init(coder: aDecoder)
+    }
+    
+    func createLocNode(){
+        let coordinate = CLLocationCoordinate2D(latitude: 37.9081200, longitude: -122.1126300)
+        let timeStamp = Date.init()
+        let location = CLLocation(coordinate: coordinate, altitude: 295, horizontalAccuracy: 10, verticalAccuracy: 10, timestamp: timeStamp)
+        let image = UIImage(named: "AppIcon")!
+
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+
     }
 }
