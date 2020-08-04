f91dac4aaeb0ec6b5201ffb6b3d673dc9baeac5f
Bennett Baker <bennettjbaker@gmail.com>
2020-08-04 11:11:12 -0700

    Map is now Explore VC, uses segmented control

---
+++ SendAR/ViewControllers/ExploreVC.swift
@@ -0,0 +1,43 @@
+//
+//  ExploreVC.swift
+//  SendAR
+//
+//  Created by Bennett Baker on 8/4/20.
+//  Copyright © 2020 Bennett Baker. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+    
+    @IBOutlet weak var exploreList: UIView!
+    @IBOutlet weak var exploreMap: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchViews(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            exploreList.alpha = 1
+            exploreMap.alpha = 0
+        } else {
+            exploreList.alpha = 0
+            exploreMap.alpha = 1
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
